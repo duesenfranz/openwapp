761c5408b7607558a2980b472b10502ae9627d61
jona <jona.klemenc@hotmail.com>
2014-12-05 22:18:59 +0100

    put all found mcc/mnc combinations in a map, so the associated country can be found easily

--- app/scripts/collections/countries.js
+++ app/scripts/collections/countries.js
@@ -10,6 +10,7 @@ define([
     model: Country,
 
     initialize: function () {
+      this.mccMncMap = new Map();
       this.fetchCountries();
     },
 
@@ -17,11 +18,27 @@ define([
       return item.get('name');
     },
 
+    addCountry: function(country) {
+      var carrier,
+        _this = this,
+        new_country = new Country({
+          carriers: country.carriers,
+          code: country.code,
+          name: country.full,
+          prefix: country.prefix
+        });
+      this.add(new_country);
+      for (carrier in country.carriers){
+        country.carriers.carrier.map(function(network) {
+          _this.mccMncMap.set(network.mcc + '-' + network.mnc, new_country);
+        })
+      }
+    },
+
     fetchCountries: function () {
       // Get from the countries.json. responseType to json is not allowed, see
       // http://updates.html5rocks.com/2012/01/Getting-Rid-of-Synchronous-XHRs
-      var xhr = new XMLHttpRequest(),
-        _this = this;
+      var xhr = new XMLHttpRequest();
       xhr.open('GET', '/scripts/countries.json', false); // sync request
       xhr.send(null);
 
@@ -40,35 +57,22 @@ define([
         } catch (e) {
           console.error('Something happened while trying to parse the JSON', e);
         }
-
-        parsed.map(function(country) {
-          _this.add(new Country({
-            carriers: country.carriers,
-            code: country.code,
-            name: country.full,
-            prefix: country.prefix
-          }));
-        });
+        parsed.map(this.addCountry);
       } else {
         console.error(xhr.statusText);
       }
     },
 
     getSelectedCountry: function (value) {
-      var result = this.find(function (model) {
+      return this.find(function (model) {
         return model.get('code') === value;
       });
-      return result;
     },
 
     getCountryByMccMnc: function (mcc, mnc) {
-      //TODO: slow! maybe make a hashmap of concentated mcc and mnc
-      return this.find(function (model) {
-        return model.hasMccMnc(mcc, mnc);
-      });
+      return this.get('mccMncMap').get(mcc + '-' + mnc)
     }
   });
 
   return Countries;
 });
-
